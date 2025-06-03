9489fff0ce1083be50ab8111e2a74ac43f2c32ab
nickmorris <nick.morris@viabledata.co.uk>
2025-06-03 14:14:58 +0100

    debug smoke test output

--- sag-docker/Test/smoke-test-local.sh
+++ sag-docker/Test/smoke-test-local.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 ## SPDX-License-Identifier: Apache-2.0
 ## © Crown Copyright 2025. This work has been developed by the National Digital Twin Programme
 ## and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
