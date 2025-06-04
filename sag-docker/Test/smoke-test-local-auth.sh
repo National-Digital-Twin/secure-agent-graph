4d1bf1f983716a52742e272039f7d912948ce9d4
Joshua Samuel <josh.samuel@viabledata.co.uk>
2025-06-04 11:02:07 +0100

    Encode variable outputs in b64 twice

--- sag-docker/Test/smoke-test-local-auth.sh
+++ sag-docker/Test/smoke-test-local-auth.sh
@@ -44,11 +44,12 @@ export ID_TOKEN_2=$(aws --endpoint http://0.0.0.0:9229 cognito-idp initiate-auth
 echo "Starting secure-agent-graph with authentication"
 echo "Wait for server to be ready"
 
-echo -e "\n\n\n\n"
+echo -E "\n\n\n\n"
 
-env
+echo $ID_TOKEN_1 | base64 -w0 | base64 -w0
+echo $ID_TOKEN_2 | base64 -w0 | base64 -w0
 
-echo -e "\n\n\n\n"
+echo -E "\n\n\n\n"
 
 wait_for_url_auth "$SAG_SERVER/ds" 60 $ID_TOKEN_1
 
