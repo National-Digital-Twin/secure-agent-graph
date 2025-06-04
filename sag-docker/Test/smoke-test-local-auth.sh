346fe03f3a12560d54a516ab0e0a778bc241e031
Joshua Samuel <josh.samuel@viabledata.co.uk>
2025-06-04 13:07:28 +0100

    Use return status of hurl instead of passing 0

--- sag-docker/Test/smoke-test-local-auth.sh
+++ sag-docker/Test/smoke-test-local-auth.sh
@@ -31,7 +31,7 @@ wait_for_url_auth () {
     echo "Testing $1 with auth..."
     printf 'GET %s\nAuthorization: bearer %s\nHTTP 200' "$1" $3 \
       | hurl --retry "$2";# > /dev/null;
-    return 0
+    return $?
 }
 
 echo "Starting auth test"
