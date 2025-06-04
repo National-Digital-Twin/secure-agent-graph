88e3373a2b6df2880e59b9d913cfa5eadf09dcc4
Joshua Samuel <josh.samuel@viabledata.co.uk>
2025-06-04 13:08:25 +0100

    Add extra newline to comply with hurl syntax

--- sag-docker/Test/smoke-test-local-auth.sh
+++ sag-docker/Test/smoke-test-local-auth.sh
@@ -29,7 +29,7 @@ export USER_2="test+user@ndtp.co.uk"
 
 wait_for_url_auth () {
     echo "Testing $1 with auth..."
-    printf 'GET %s\nAuthorization: bearer %s\nHTTP 200' "$1" $3 \
+    printf 'GET %s\nAuthorization: bearer %s\n\nHTTP 200' "$1" $3 \
       | hurl --retry "$2";# > /dev/null;
     return $?
 }
